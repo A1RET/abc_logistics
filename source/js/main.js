db786b9bf08e77d9370001a27a9e24e9fe025b78
Airet <thiefon@gmail.com>
2019-06-09 22:35:31 +0500

    Добавил модальное окно с формой звонка

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,10 @@
 (function($){
-$(window).on("load",function() {
   /*----Кастомный скроллбар для таблицы---*/
   $(".table-wrapper").mCustomScrollbar({
     axis:"x", ///Включение горизонтального скроллбара
     scrollInertia: 0, ///Отключение анимации
     theme: "my-theme" ///Стили
   });
-});
+
+  $(".modal-content").load("call.html #content");
 })(jQuery);
