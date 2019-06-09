4fbc369526db323e399166efd17e779c18b1eba1
Airet <thiefon@gmail.com>
2019-06-09 23:52:16 +0500

    Добавил скрипт на переключение модального окна

--- source/js/main.js
+++ source/js/main.js
@@ -6,5 +6,15 @@
     theme: "my-theme" ///Стили
   });
 
+  var toggleModal = function(evt) {
+    event.preventDefault();
+    $(".modal").toggle();
+    $(".modal-overlay").toggle();
+    event.stopPropagation();
+  }
+  $(".call-btn").click(toggleModal);
+  $(".close-modal").click(toggleModal);
+  $(".modal-overlay").click(toggleModal);
+
   $(".modal-content").load("call.html #content");
 })(jQuery);
