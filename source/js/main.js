d9b9fd597e32704dfaadb6beda30383d0dc4de0e
Airet <thiefon@gmail.com>
2019-06-10 00:55:41 +0500

    Добавил скрипт открытия мобильного меню

--- source/js/main.js
+++ source/js/main.js
@@ -6,15 +6,40 @@
     theme: "my-theme" ///Стили
   });
 
+  /*---Функция переключения элементов мобильного меню---*/
+  var toggleMenu = function(evt) {
+    event.preventDefault();
+    $(".header-logo").toggleClass("header-logo-modal");
+    $(".nav-site").toggleClass("nav-site-modal");
+    $(".nav-btn").toggleClass("nav-btn-opened");
+  };
+  /*---Переключение меню по кнопке---*/
+  $(".nav-btn").click(toggleMenu);
+
+  /*---Выключение мобильного меню на десктопе---*/
+  $(window).resize(function () {
+    if($(window).width() >= 1020) {
+    $(".header-logo").removeClass("header-logo-modal");
+    $(".nav-site").removeClass("nav-site-modal");
+    $(".nav-btn").removeClass("nav-btn-opened");
+    };
+  });
+
+  /*---Функция переключения модального окана с формой---*/
   var toggleModal = function(evt) {
     event.preventDefault();
     $(".modal").toggle();
     $(".modal-overlay").toggle();
     event.stopPropagation();
   }
-  $(".call-btn").click(toggleModal);
-  $(".close-modal").click(toggleModal);
-  $(".modal-overlay").click(toggleModal);
 
+  /*---Переключение модального окна---*/
+  /*---При клике на кнопку Заказать звонок---*/
+  $(".call-btn").click(toggleModal);
+  /*---При клике на кнопку Закрыть окно---*/
+  $(".close-modal").click(toggleModal);
+  /*---При клике на оверлей---*/
+  $(".modal-overlay").click(toggleModal);
+  /*---Загрузка формы в модальном окне---*/
   $(".modal-content").load("call.html #content");
 })(jQuery);
